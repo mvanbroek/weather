9149b55445886ae0ea52bdc3ac91322fdb909c3a
mvanbroek <m-vanbroek@kellogg.northwestern.edu>
2016-02-03 14:59:00 -0600

    Wednesady

--- weather.js
+++ weather.js
@@ -9,7 +9,8 @@ $(function() {
     + data.currently.summary +
     "<p> Tomorrow: " + "High - " + data.daily.data[0].temperatureMax + "&deg;, " + "Low - " +data.daily.data[0].temperatureMin + "&deg;, " + data.daily.data[0].summary +
     "<p> The Day After Tomorrow: " + "High - " + data.daily.data[1].temperatureMax + "&deg;, " + "Low - " + data.daily.data[1].temperatureMin + "&deg;, " + data.daily.data[1].summary +
-    "<p> The Day After the Day After Tomorrow: " + "High - " + data.daily.data[2].temperatureMax + "&deg;, " + "Low - " + data.daily.data[1].temperatureMin + "&deg;, " + data.daily.data[2].summary;
+    "<p> The Day After the Day After Tomorrow: " + "High - " + data.daily.data[2].temperatureMax + "&deg;, " + "Low - " + data.daily.data[1].temperatureMin + "&deg;, " + data.daily.data[2].summary
+    ;
 
 
 
